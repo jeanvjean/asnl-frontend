c96ea50daff4dd12757ce022e93968fc134138d8
Inim Andrew <inimandrew@gmail.com>
2021-07-01 05:51:29 +0100

    milestone 2

--- pages/dashboard/cylinder-management/condemn.vue
+++ pages/dashboard/cylinder-management/condemn.vue
@@ -102,6 +102,34 @@
             </svg>
             <span>Condemn</span>
           </router-link>
+          <router-link
+            to="/dashboard/cylinder-management/cylinder-type"
+            class="
+              flex
+              space-x-4
+              items-center
+              text-btn-purple
+              border-btn-purple border
+              px-4
+              py-2
+              rounded-sm
+              md:mx-auto
+            "
+          >
+            <svg
+              class="w-6 h-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            <span class="text-center">Cylinder Type</span>
+          </router-link>
         </div>
       </div>
     </div>
