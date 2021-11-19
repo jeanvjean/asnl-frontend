7aa135858d2ecc69ed73ae93acd26886788afe09
Chidike <chidikenwandu@gmail.com>
2021-11-19 06:55:49 +0100

    cylinder transfer

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,11 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    head: {
+      script: [{
+        src: "JsBarcode.all.min.js",
+      }],
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
